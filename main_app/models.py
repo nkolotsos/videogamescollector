789f9719c7c2d3ae5cd0a75358bfb122b2f8bb9c
nkolotsos <nkolotsos@gmail.com>
2023-08-28 12:29:09 +1000

    Lab Part 4 complete

--- main_app/models.py
+++ main_app/models.py
@@ -17,5 +17,33 @@ class Videogame(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse('videogames_detail', kwargs={'videogame_id': self.id})
+        return reverse('videogames_detail', kwargs={'videogames_id': self.id})
 
+class Review(models.Model):
+    name = models.CharField(max_length=100)
+    comment = models.TextField(max_length=500)
+    date = models.DateField(auto_now_add=True)
+    videogame = models.ForeignKey(Videogame, related_name="reviews", on_delete=models.CASCADE)
+
+    RATING_CHOICES = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+    )
+
+    rating = models.IntegerField(choices=RATING_CHOICES, default=3)
+
+    def __str__(self):
+        return f"{self.name} @ {self.date}"
+    
+    def get_absolute_url(self):
+        return reverse('add_review', kwargs={'videogame_id': self.id})
+
+    @property
+    def rating_display(self):
+        return dict(self.RATING_CHOICES).get(self.rating)
+    
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Videogame
+from .models import Videogame, Review
+from .forms import ReviewForm
 
 # Create your views here.
 def home(request):
@@ -16,7 +17,16 @@ def videogames_index(request):
 
 def videogames_detail(request, videogames_id):
     videogame = Videogame.objects.get(id=videogames_id)
-    return render(request, 'videogames/detail.html', { 'videogame': videogame })
+    review_form = ReviewForm()
+    return render(request, 'videogames/detail.html', { 'videogame': videogame, 'reivew_form': review_form})
+
+def add_review(request, videogames_id):
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        review = form.save(commit=False)
+        review.videogames_id = videogames_id
+        review.save()
+    return redirect('videogames_detail', videogames_id=videogames_id)
 
 class VideogameCreate(CreateView):
     model = Videogame
@@ -32,3 +42,4 @@ class VideogameDelete(DeleteView):
     model = Videogame
     success_url = '/videogames'
     template_name = 'main_app/videogame_confirm_delete.html'
+
